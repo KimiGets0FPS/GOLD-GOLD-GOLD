d12283db2ee52fd101b0db98896c45244eb60c2e
Zhewei Wan <zheweiwan@gmail.com>
2025-05-15 01:01:37 -0700

    quick boom

--- CMs/ColorPrint.java
+++ CMs/ColorPrint.java
@@ -3,6 +3,12 @@ package CMs;
 import java.awt.Color;
 
 public class ColorPrint {
+    public static final String LIGHT_BLUE_TEXT  = "\u001B[94m";
+    public static final String BLUE_TEXT = "\u001B[34m";
+    public static final String PURPLE_TEXT = "\u001B[35m";
+    public static final String GOLD_TEXT = "\u001B[33m";
+    public static final String RESET_TEXT = "\u001B[0m";
+
     public static void main(String[] args) {  // Testing
 
     }
@@ -11,5 +17,7 @@ public class ColorPrint {
 
     }
 
+    public static void PullAnimation() {
 
+    }
 }
